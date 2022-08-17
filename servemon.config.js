b4b6d4ad24d7da8b15bf07e8520f3a77d9508e09
lassev05 <vestergaardlasse2@gmail.com>
2022-08-17 15:36:22 +0200

    Create project

---
+++ servemon.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  projectName: "budget-app",
+  port: 1910, // Port to use
+  directory: "./src", // Directory to use
+  //  pkgManager: "pnpx", // Package manager you want to use
+  watch: true, // Watch for changes
+  open: false, // Open the browser
+  fileExplore: false, // Enable file explorer
+  logger: false, // Enable logger
+};
